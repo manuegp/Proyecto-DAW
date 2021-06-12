f89c7b99dc9c8e39db5806900d07032b4e3ca057
DESKTOP-SH2V356\Kapryx <mario301298@gmail.com>
2021-06-12 12:05:01 +0200

    Documentado la parte del login

--- Frontend/Proyecto-DAW/src/app/login/autentication-profile.guard.ts
+++ Frontend/Proyecto-DAW/src/app/login/autentication-profile.guard.ts
@@ -6,22 +6,33 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AutenticationProfileGuard implements CanActivate {
-  constructor (private router:Router){}
+
+  //---------------------------------------------------------
+  //--Constructor---------
+  //---------------------------------------------------------
+  constructor(private router: Router) { }
+
+  //---------------------------------------------------------
+  //---------Funciones---------------------------------------
+  //---------------------------------------------------------
+
+  //Funcion que utilizo en el fichero app-routing.module.ts para acceder a las rutas profile y shopping-cart
+  //en caso de que exista un localStorage de usuario, es decir en caso de que el usuario haya iniciado sesion.
+  //Si no ha iniciado sesion, le redirigira al home
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    
-      if (localStorage.getItem("usuario")) {
-        return true;
-        
-       
-        
-      }else{
-      this.router.navigate(['/home'], {queryParams:{urlRespuesta:state.url}});
-      console.log("No estas loggeado");
+
+    if (localStorage.getItem("usuario")) {
+      return true;
+
+
+
+    } else {
+      this.router.navigate(['/home'], { queryParams: { urlRespuesta: state.url } });
       return false
-      
-      }
+
+    }
   }
-  
+
 }
--- Frontend/Proyecto-DAW/src/app/login/login.component.ts
+++ Frontend/Proyecto-DAW/src/app/login/login.component.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
-import { catchError } from 'rxjs/operators';
-import { error } from 'selenium-webdriver';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 import { ModificarProductoComponent } from '../componentesAdmin/modificar-producto/modificar-producto.component';
 import { AutenticacionService } from './autenticacion.service';
@@ -13,61 +12,65 @@ import { AutenticacionService } from './autenticacion.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent  {
-  errorCredeciales: boolean = false;
-  login: FormGroup ;
-  urlLogin ="http://localhost:8000/api/tokens/create";
-  constructor(
-    private _builder : FormBuilder,
-    private http:HttpClient,
-    private dialogRef: MatDialogRef<ModificarProductoComponent>,
-    private autenticacionServe:AutenticacionService
+export class LoginComponent {
 
-  ) { 
+  //---------------------------------------------------------
+  //---------Variables---------------------------------------
+  //---------------------------------------------------------
+  errorCredeciales: boolean = false;
+  login: FormGroup;
+
+  //---------------------------------------------------------
+  //--Constructor/Funciones de inicio del componente---------
+  //---------------------------------------------------------
+  constructor(
+    private _builder: FormBuilder,
+    private http: HttpClient,
+    private dialogRef: MatDialogRef<ModificarProductoComponent>,
+    private autenticacionServe: AutenticacionService,
+    private router: Router
+
+  ) {
     this.login = this._builder.group({
-      email:['', Validators.required],
+      email: ['', Validators.required],
       password: ['', Validators.required]
     })
   }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void { }
+
+
+  //---------------------------------------------------------
+  //---------Funciones---------------------------------------
+  //---------------------------------------------------------
+
+  submit(event: any) {
+
+    //Ejecuto la funcion login, el cual se encuentra en autenticacion.service.ts; y con los datos que devuelve,
+    //creo el localStorage usuario para guardar los datos del usuario loggeado
+    this.autenticacionServe.login(this.login.controls.email.value, this.login.controls.password.value).subscribe(datos => {
+      localStorage.setItem("usuario", JSON.stringify(datos));
+      this.dialogRef.close()
+
+      //Si existe el localStorage "password", indicando que ha pedido cambiar la contraseña pero no la ha cambiado
+      //cuando se logee se quitará 
+      if (localStorage.getItem("password")) {
+        localStorage.removeItem("password");
+      }
+
+      this.router.navigate(['home']); //Redirige a home
+
+
+    },
+      error => {
+        console.log(error)
+        this.errorCredeciales = true
+      }
+    ),
+      (error: any) => console.log(error);
+
+
   }
-
-  
-
- 
-  
-
- 
-
- submit(event : any) {
-
-  if (this.login.invalid) {
-    console.log("adios");
-    
-  }
-
-  if (localStorage.getItem("password")) {
-    localStorage.removeItem("password");
-  }
-
-  this.autenticacionServe.login(this.login.controls.email.value, this.login.controls.password.value).subscribe(datos=>{
-    localStorage.setItem("usuario", JSON.stringify(datos));
-    console.log(datos);
-    this.dialogRef.close()
-    
-    
-  }, 
-  error =>{
-    console.log(error)
-    this.errorCredeciales = true
-  }
-  ),
-    (  error: any) => console.log(error);
-
-  
-}
 
 }
 
